6e340dacfb721cb8c967c8a0a4a21f859d5d51ac
mariana-perezh <mperezh@email.unc.edu>
2022-11-20 00:03:30 -0500

    server.js

--- server.js
+++ server.js
@@ -10,3 +10,33 @@ var port = 5000;
 if(args.port) {
 	port = args.port;
 }
+
+var sides = 6;
+var dice = 2;
+var rolls = 1;
+
+app.get('/app/', (req, res) => {
+	res.send("200 OK");
+}
+
+app.get('/app/roll/', (req, res, next) => {
+	res.send(roll(sides, dice, rolls));
+})
+
+app.get('/app/roll/:sides', (req, res, next) => {
+	sides = parseInt(req.params.sides);
+	res.send(roll(sides, dice, rolls)).end();
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res, next) => {
+	sides = parseInt(req.params.sides);
+	dice = parseInt(req.params.dice);
+	res.send(roll(sides, dice, rolls)).end();
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
+	sides = parseInt(req.params.sides);
+	dice = parseInt(req.params.dice);
+	rolls = parseInt(req.params.rolls);
+	res.send(roll(sides, dice, rolls)).end();
+})
